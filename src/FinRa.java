d867be9e2109fef8b50d3bfeea3638417fd49e2a
Yusif <yusif.h@hotmail.com>
2022-09-29 23:56:03 -0400

    Finra task completed

---
+++ src/FinRa.java
@@ -0,0 +1,24 @@
+public class FinRa {
+    public static void main(String[] args) {
+
+
+
+    finRa();
+
+}
+
+    public static void finRa(){
+        int num = 1;
+        while (num <=30){
+            if(num%3==0 && num%5==0){
+                System.out.println(num + ":<FINRA>");
+            }else if(num%3==0){
+                System.out.println(num + ": <FIN>");
+            }else if(num%5==0){
+                System.out.println(num + ": <RA>");
+
+            }
+            num++;
+        }
+    }
+}
