fb7d17dfb73145c6ff954c750fa0bc8cd1718a66
Yusif <yusif.h@hotmail.com>
2022-09-30 11:09:40 -0400

    DivideMethod class completed

--- src/DivideMethod.java
+++ src/DivideMethod.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class DivideMethod {
+    public static void main(String[] args) {
+
+        divideMethod();
+
+    }
+
+    public static void divideMethod(){
+        int a,b,c;
+        int result=0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        a = input.nextInt();
+        System.out.println("Enter 2nd number");
+        b = input.nextInt();
+        c = a + b;
+
+        while (c>=b){
+            c=c-b;
+            result++;
+        }
+
+        System.out.println("result = " + result);
+    }
 }
