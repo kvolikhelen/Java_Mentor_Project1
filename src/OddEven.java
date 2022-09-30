ff4c0afa991d99a0300f10d1291c25a15ddda6a9
Yusif <yusif.h@hotmail.com>
2022-09-30 11:07:55 -0400

    EvenOdd class completed

--- src/OddEven.java
+++ src/OddEven.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class OddEven {
+    public static void main(String[] args) {
+
+        evenOddNum();
+
+
+    }
+
+    public static void evenOddNum() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " is even number");
+        }else{
+            System.out.println(number + " is odd number");
+        }
+
+
+    }
 }
