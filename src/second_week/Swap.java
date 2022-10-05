f8acc1fea1bbe6629255e201e3d19a23de39757c
Yusif <yusif.h@hotmail.com>
2022-10-05 00:59:38 -0400

    swap class completed

--- src/second_week/Swap.java
+++ src/second_week/Swap.java
@@ -2,4 +2,18 @@ package second_week;
 
 public class Swap {
 
+    //Swap two variables values without using third variable
+
+    public static String swap(int a, int b){
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        return "a swap to b: a = " + a +" b = " + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(swap(3, 5));
+    }
+
 }
