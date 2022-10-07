87ab07378220fe9002679909428d6d2a38e9ff84
shivaniharjani <shivanih06@gmail.com>
2022-10-07 10:56:58 -0400

    Shivani - week1 tasks

---
+++ src/Week1/EvenOdd.java
@@ -0,0 +1,24 @@
+package Week1;
+
+public class EvenOdd {public static void main(String[] args) {
+
+    evenOrOdd(6);
+
+}
+
+
+
+    public static void evenOrOdd(int number){
+
+
+        if(number % 2 == 0){
+            System.out.println(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
+        }
+
+
+    }
+
+
+}
