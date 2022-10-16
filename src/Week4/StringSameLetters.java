c64a6a896f0797641cbb100941343ff09f218ded
huseyin <huseyinisir07@gmail.com>
2022-10-16 12:33:33 -0400

    Huseyin - WEEK-4

--- src/Week4/StringSameLetters.java
+++ src/Week4/StringSameLetters.java
@@ -1,6 +1,6 @@
 package Week4;
 
-import java.util.Arrays;
+import java.util.*;
 
 /*
 String -- Same letters
@@ -11,37 +11,60 @@ same("abc", "abb"); -> false:
 
  */
 public class StringSameLetters {
-    // sameLetter by using Array and Array methods
+    // sameLetter by using Collection framework, Iterator
     public static boolean sameLetter(String str, String str1){
-        if (str.length() == str1.length()){
-            String[] arr = str.split("");
-            String[] arr1 = str1.split("");
-            Arrays.sort(arr);
-            Arrays.sort(arr1);
-            return Arrays.equals(arr,arr1);
+
+        TreeSet<String> list = new TreeSet<>(Arrays.asList(str.split("")));
+        TreeSet<String> list1 = new TreeSet<>(Arrays.asList(str1.split("")));
+
+        if (list.size() != list1.size()){
+            return false;
+        }else {
+            Iterator<String> letters = list.iterator();
+            Iterator<String> letters1 = list1.iterator();
+            while (!letters.next().equals(letters1.next())){
+                return false;
+            }
         }
-        return false;
+
+        return true;
     }
 
     // sameLetter by using String methods and loop
     public static boolean sameLetter1(String str, String str1){
-        if (str.length() != str1.length()){
-            return false;
-        }
-        boolean result = true;
+
+        String temp = "";
         for (int i = 0; i < str.length(); i++) {
-            if (!str.contains("" + str1.charAt(i)) || !str1.contains("" + str.charAt(i))){
-                return false;
+            if (!temp.contains("" + str.charAt(i))){
+                temp += "" + str.charAt(i);
             }
         }
-        return result;
+
+        String temp1 = "";
+        for (int i = 0; i < str1.length(); i++) {
+            if (!temp1.contains("" + str1.charAt(i))){
+                temp1 += "" + str1.charAt(i);
+            }
+        }
+
+        if (temp.length() != temp1.length()){
+            return false;
+        }else{
+            for (int i = 0; i < temp.length(); i++) {
+                if (!temp.contains("" + temp1.charAt(i)) || !temp1.contains("" + temp.charAt(i))){
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(sameLetter("abc", "cab"));
+        System.out.println(sameLetter("aaabbbccdd", "abcd"));
         System.out.println(sameLetter("abc", "abb"));
 
-        System.out.println(sameLetter1("abc", "cab"));
+        System.out.println(sameLetter1("aaabbbccdd", "abcd"));
         System.out.println(sameLetter1("abc", "abb"));
 
     }
