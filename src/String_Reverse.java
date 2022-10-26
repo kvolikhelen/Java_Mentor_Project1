80b38e66cff7e50ab72cdd87b7345e1002f05023
Marko Stevanovic <mstevanovic92@yahoo.com>
2022-10-26 10:34:01 -0400

    Week 5,Marko

---
+++ src/String_Reverse.java
@@ -0,0 +1,27 @@
+public class String_Reverse {
+
+    //String -- Reverse
+    //Write a return method that can reverse String
+    //Ex: Reverse("ABCD"); ==> DCBA
+
+
+    public static void main(String[] args) {
+        System.out.println("StrReverse() = " + StrReverse("ABCD"));
+
+    }
+
+    public static String StrReverse(String str) {
+
+        String reverse="";
+
+        for(int i=str.length()-1; i >= 0; i--) {
+            reverse += str.toCharArray()[i];
+        }
+        return reverse;
+
+    }
+
+
+
+
+}
