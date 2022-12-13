258ee9ab8fc2a6b1e02a11bd0e54972869d80c3f
Husna <ozyildirimhusna@gmail.com>
2022-12-13 08:19:24 -0500

    week11 done

--- src/week11/MapMinValue.java
+++ src/week11/MapMinValue.java
@@ -1,11 +1,29 @@
 package week11;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapMinValue {
     /*Map -- Min value
 Write a method that can return the minimum value from ta map (DO NOT use sort method)*/
-    public static int minOfMap(Map<Integer,Integer> map){
-        return 0;
+    public static int minOfMap(Map<Integer,String> map){
+        Map.Entry<Integer , String> minEntry = null;
+
+        for (Map.Entry<Integer , String > entry : map.entrySet()) {
+
+            if (minEntry == null || minEntry.getKey() > entry.getKey() ){
+                minEntry = entry;
+            }
+        }
+
+     return minEntry.getKey();
+
+    }
+
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>(Map.of(4,"four",1,"one",2,"two",5,"five"));
+
+        System.out.println(minOfMap(map));
     }
 }
--- src/week11/UniqueCharacter.java
+++ src/week11/UniqueCharacter.java
@@ -1,8 +1,6 @@
 package week11;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class UniqueCharacter {
     /*Map -- Unique character from String
@@ -11,22 +9,19 @@ For example:
 “aabccddeffft”
 {b=1,e=1,t=1}*/
     public static Map<Character,Integer> uniqueCharacter(String str){
-        Map<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> map = new LinkedHashMap<>();
         for (char c : str.toCharArray())
             if (map.containsKey(c)) {
                 map.put(c, map.get(c) + 1);
             } else {
                 map.put(c, 1);
             }
-        for (Map.Entry<Character, Integer> each : map.entrySet()) {
-            if(each.getValue() > 1 )
-            { map.remove(each.getKey(),each.getValue()); }
-        }
-
+        map.values().removeIf( value -> value>1 );
         return map;
     }
 
     public static void main(String[] args) {
-        System.out.println(uniqueCharacter("abbbccdddefgh"));
+        System.out.println(uniqueCharacter("aabccddeffft"));
+
     }
 }
